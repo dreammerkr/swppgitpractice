6ba8677809d661668cc9176a0c45545681c4f3ed
Kwaktaewon <tykwak94@gmail.com>
2019-09-16 18:06:37 +0900

    add world.py

---
+++ world.py
@@ -0,0 +1,17 @@
+
+# world.py file
+import time
+
+def hello_world():
+	print("hello wolrd")
+
+def bye_world():
+	time.sleep(2)
+	print("bye world")
+
+if __name__ == "__main__":
+	hello_world()
+	bye_world() 
+
+
+
